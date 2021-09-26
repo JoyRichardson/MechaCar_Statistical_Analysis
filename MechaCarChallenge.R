2afff16599542eb7514decf110a6091c44dd5ac9
JoyRichardson <84511019+JoyRichardson@users.noreply.github.com>
2021-09-26 09:21:36 -0500

    Commit Deliverable2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# Deliverable 1
+
 # Use the library() function to load the dplyr package
 library(dplyr)
 
@@ -10,4 +12,33 @@ mecha_mpg <- read.csv(file='./MechaCar_mpg.csv',check.names=F,stringsAsFactors =
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
 
 # Using the summary() function, determine the p-value and the r-squared value for the linear regression model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg))
+
+
+# Deliverable 2
+
+# Import and read in the Suspension_Coil.csv file as a table
+mecha_coil <- read.csv(file='./Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a total_summary dataframe using the summarize() function to get the mean, median, variance,
+# and standard deviation of the suspension coil's PSI column
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Num_Coil=n(), .groups = 'keep') 
+
+# Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot
+# by the mean, median, variance, and standard deviation of the suspension coil's PSI column
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                         Var_PSI=var(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep')     
+
+
+
+
+
+
+
